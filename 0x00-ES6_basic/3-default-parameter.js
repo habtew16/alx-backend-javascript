c09ac0dcc0a881ecdbfa291d688309e787772cfd
habtew16 <habtamuwolde96@gmail.com>
2024-04-08 17:19:39 +0000

    param

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,4 @@
+// needed some comment for param
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
